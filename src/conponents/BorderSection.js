19eab3393bf1488d272c2511aa873756b24462eb
Riz <mahmoud.eleresousy@gmail.com>
2023-12-09 04:27:53 +0200

    Apply responsive styling to projects page

--- src/conponents/BorderSection.js
+++ src/conponents/BorderSection.js
@@ -10,9 +10,11 @@ const BorderSection = ({
   return (
     <article
       className={classNames(
-        'w-full h-full flex items-center justify-between relative',
+        'w-full h-full flex items-start justify-between relative',
         'rounded-br-2xl rounded-3xl border border-solid p-12',
-        'border-dark bg-light hover:shadow-2xl dark:hover:shadow-light dark:bg-dark dark:border-light dark:hover:shadow-lg',
+        'border-dark bg-light hover:shadow-2xl dark:hover:shadow-light',
+        'dark:bg-dark dark:border-light dark:hover:shadow-xl',
+        'lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4',
         className
       )}
     >
@@ -20,7 +22,8 @@ const BorderSection = ({
       {shadow && (
         <div
           className={classNames(
-            'absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light',
+            'absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] rounded-br-3xl bg-dark dark:bg-light',
+            'md:-right-1 lg:h-[102%] md:w-full md:rounded-[1.5rem]',
             childrenClassName
           )}
         />
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
+import classNames from 'classnames';
+import { motion } from 'framer-motion';
 
 import { GithubIcon } from '@/conponents/icons';
 import project1 from '../../public/images/projects/portfolio-cover-image.jpg';
 import BorderSection from '@/conponents/BorderSection';
 import Layout from '@/conponents/Layout';
 import AnimatedText from '@/conponents/AnimatedText';
-import classNames from 'classnames';
+
+const FramerImage = motion(Image);
 
 const Project = ({
   type,
@@ -20,62 +23,80 @@ const Project = ({
   skills,
   small = false,
 }) => {
+
   return (
     <BorderSection
       className={classNames({
-        'flex-col justify-center !rounded-2xl shadow-none p-6': small,
+        'flex-col justify-center !rounded-2xl p-6 dark:hover:shadow-[5px_5px_13px_light] xs:p-4': small,
       })}
       shadow={small ? false : true}
     >
       <Link
-        href={link}
+        href={link || github}
         target='_blank'
-        className={classNames('w-1/2 h-full cursor-pointer overflow-hidden rounded-lg', {'!w-full !h-auto': small})}
+        className={classNames(
+          'w-1/2 h-auto cursor-pointer overflow-hidden rounded-lg lg:w-full',
+          { '!w-full': small }
+        )}
       >
-        <Image src={img} alt={title} className={classNames({'w-full h-auto':small, 'h-full w-auto': !small})} 
-                          priority
-                          sizes='(min-width: 768px) 100vw, (min-width: 1200px) 50vw, 50vw'
-                          />
+        <FramerImage
+          src={img}
+          alt={title}
+          className='w-full h-auto'
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          sizes='(min-width: 768px) 100vw, (min-width: 1200px) 50vw, 50vw'
+          priority
+        />
       </Link>
       <div
         className={classNames(
-          'w-1/2 flex flex-1 flex-col items-start justify-between pl-6 ',
-          { 'pl-0 mt-4 !w-full': small }
+          'w-1/2 flex flex-1 flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6 h-full',
+          { '!pl-0 mt-4 !w-full lg:text-lg md:text-base': small }
         )}
       >
-        <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark sm:text-sm'>
+          {type}
+        </span>
         <Link
-          href={link}
+          href={link || github}
           target='_blank'
           className='hover:underline underline-offset-2'
         >
           <h2
-            className={classNames('my-2 w-full text-left text-4xl font-bold', {
-              'text-3xl': small,
+            className={classNames('my-2 w-full text-left text-4xl font-bold sm:text-base', {
+              'text-3xl lg:text-2xl': small,
             })}
           >
             {title}
           </h2>
         </Link>
 
-        {summary && <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>}
-        <ul className='mb-2 flex justify-start items-center flex-wrap '>
+        {summary && (
+          <p className='my-2 font-medium text-dark dark:text-light sm:text-sm'>
+            {summary}
+          </p>
+        )}
+        <ul className='mb-4 flex justify-start items-center flex-wrap'>
           {skills?.map((skill, i) => (
-            <li  key={skill + '-' + i} className='text-sm text-dark/75 dark:text-light/75'>
+            <li
+              key={skill + '-' + i}
+              className='text-sm text-dark/75 dark:text-light/75'
+            >
               {skill}
               {i < skills.length - 1 && <>, &nbsp;</>}
             </li>
           ))}
         </ul>
         <div
-          className={classNames('mt-2 flex items-center', {
-            'w-full flex-row-reverse justify-between mt-auto': small,
+          className={classNames('flex items-center mt-auto', {
+            'w-full flex-row-reverse justify-between': small,
           })}
         >
           <Link
             href={github}
             target='_blank'
-            className={classNames('w-10', { 'w-8': small })}
+            className={classNames('w-10', { 'w-8 md:w-6': small })}
           >
             <GithubIcon />
           </Link>
@@ -83,9 +104,10 @@ const Project = ({
             <Link
               href={link}
               target='_blank'
-              className={classNames('font-semibold underline', {
-                'ml-4 rounded-lg bg-dark text-light p-1 px-6 text-lg no-underline dark:bg-light dark:text-dark':
-                  !small,
+              className={classNames('font-semibold', {'md:text-base hover:underline': small,
+                'ml-4 rounded-lg bg-dark hover:bg-dark/75 text-light p-1 px-6 text-lg': !small,
+                'dark:bg-light dark:hover:bg-light/75 dark:text-dark sm:px-4 sm:text-base': !small,
+                
               })}
             >
               Live Demo
@@ -107,13 +129,13 @@ const projects = () => {
           content='Mahmoud Rizk Portfolio. About and Experience Section.'
         />
       </Head>
-      <main className='w-full mb-16 flex flex-col items-center justify-center'>
+      <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
           <AnimatedText
             text='Imagination Trumps Knowledge!'
-            className='mb-16'
+            className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'
           />
-          <div className='grid grid-cols-12 gap-24'>
+          <div className='grid grid-cols-12 gap-x-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 sm:gap-y-16'>
             <div className='col-span-12'>
               <Project
                 title='Crypto Screener Application'
@@ -134,7 +156,7 @@ const projects = () => {
               />
             </div>
 
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                 title='Crypto Screener Application'
                 type='Featureed Project'
@@ -155,11 +177,10 @@ const projects = () => {
                 ]}
               />
             </div>
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                 title='Crypto Screener Application'
                 type='Featureed Project'
-                link='/'
                 github='/'
                 small={true}
                 img={project1}
