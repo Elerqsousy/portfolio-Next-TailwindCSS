3c6606dff5a13acb6f269ee0bbed0de8f724552d
Riz <mahmoud.eleresousy@gmail.com>
2023-12-09 10:26:19 +0200

    Integrate form and form context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Montserrat } from 'next/font/google';
 import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
@@ -8,6 +9,8 @@ import '@/styles/globals.css';
 
 import Footer from '@/conponents/Footer';
 import NavBar from '@/conponents/NavBar';
+import ContactForm from '@/conponents/ContactForm';
+import { ContactFormContext } from '@/conponents/Context';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -16,6 +19,8 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <>
       <Head>
@@ -26,11 +31,14 @@ export default function App({ Component, pageProps }) {
         className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
       >
         <ThemeProvider enableSystem={true} attribute='class'>
-          <NavBar />
-          <AnimatePresence mode='wait'>
-            <Component key={router.asPath} {...pageProps} />
-          </AnimatePresence>
-          <Footer />
+          <ContactFormContext.Provider value={{ isOpen, setIsOpen }}>
+            <ContactForm />
+            <NavBar />
+            <AnimatePresence mode='wait'>
+              <Component key={router.asPath} {...pageProps} />
+            </AnimatePresence>
+            <Footer />
+          </ContactFormContext.Provider>
         </ThemeProvider>
       </main>
     </>
